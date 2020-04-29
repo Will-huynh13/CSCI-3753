8dca9a8a6a2fdeaa069baea65f9af54f92f34c67
Will <wihu4176@colorado.edu>
2020-04-28 20:27:29 -0600

    Modified C files

--- Programming Assignment 4/pager-lru.c
+++ Programming Assignment 4/pager-lru.c
@@ -18,8 +18,8 @@
 
 #include "simulator.h"
 
-void pageit(Pentry q[MAXPROCESSES]) { 
-    
+void pageit(Pentry q[MAXPROCESSES])
+{ 
     /* This file contains the stub for an LRU pager */
     /* You may need to add/remove/modify any part of this file */
 
@@ -31,21 +31,48 @@ void pageit(Pentry q[MAXPROCESSES]) {
     /* Local vars */
     int proctmp;
     int pagetmp;
+    int page_LRU;
+    int oldest_page;
 
     /* initialize static vars on first run */
-    if(!initialized){
-	for(proctmp=0; proctmp < MAXPROCESSES; proctmp++){
-	    for(pagetmp=0; pagetmp < MAXPROCPAGES; pagetmp++){
-		timestamps[proctmp][pagetmp] = 0; 
+    if(!initialized)
+    {
+	    for(proctmp = 0; proctmp < MAXPROCESSES; proctmp++)
+        {
+	        for(pagetmp = 0; pagetmp < MAXPROCPAGES; pagetmp++)
+            {
+		        timestamps[proctmp][pagetmp] = 0; 
+	        }
 	    }
-	}
-	initialized = 1;
+	    initialized = 1;
     }
-    
-    /* TODO: Implement LRU Paging */
-    fprintf(stderr, "pager-lru not yet implemented. Exiting...\n");
-    exit(EXIT_FAILURE);
 
+    // beginning of LRU loop
+    for (proctmp = 0; proctmp < MAXPROCESSES; proctmp++) // looping thorugh the number of processes
+    {
+        if (q[proctmp].active)  // checking if the process is active
+        {
+            pagetmp = q[proctmp].pc / PAGESIZE; // assigning the current page/ getting process current page
+
+            if(!q[proctmp].pages[pagetmp] && !pagein(proctmp,pagetmp)) //checking if the page is swapped in
+            {
+                // need to set the oldest page to the tick
+                oldest_page = tick; // like minimum tick in the page
+                for (int i = 0; i < MAXPROCPAGES; i++)
+                {
+                    if (q[proctmp].pages[i] && timestamps[proctmp][i] < oldest_page) // if the page at i is already swapped and the page is not the oldest
+                    {
+                        oldest_page = timestamps[proctmp][i]; // reassigning the minimum tick
+
+                        page_LRU = i; // the current page is set to the index to be removed
+                    }
+                }
+                pageout(proctmp,page_LRU); // removes the current page
+                break;
+            }
+            timestamps[proctmp][pagetmp] = tick; // sets the current process and current page to the current tick
+        }
+    }
     /* advance time for next pageit iteration */
     tick++;
 } 
--- Programming Assignment 4/pager-predict.c
+++ Programming Assignment 4/pager-predict.c
@@ -12,35 +12,39 @@
  * 	This file contains a predictive pageit
  *      implmentation.
  */
-
+// resources used: Google for basic LRU info. Colaborated with agus with R files
 #include <stdio.h> 
 #include <stdlib.h>
-
 #include "simulator.h"
-
-void pageit(Pentry q[MAXPROCESSES]) { 
-    
-    /* This file contains the stub for a predictive pager */
-    /* You may need to add/remove/modify any part of this file */
-
-    /* Static vars */
-    static int initialized = 0;
-    static int tick = 1; // artificial time
-    
-    /* Local vars */
-    
-
-    /* initialize static vars on first run */
-    if(!initialized){
-	/* Init complex static vars here */
-	
-	initialized = 1;
+void pageit(Pentry q[MAXPROCESSES])
+{ 
+    int page;
+    int CurProcess = 0;
+    while(CurProcess < MAXPROCESSES)
+    {
+        if (q[CurProcess].active) // checking if process is active
+        {
+            int myPrediction[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // seeting the predicton array to all 0, meaning false
+            page = q[CurProcess].pc / PAGESIZE; // grabbing the page (calculation the page)
+            myPrediction[page] = 1; // in the prediction array, current page is paged in
+            if (page == 2){myPrediction[4] = 1;myPrediction[3] = 1;myPrediction[0] = 1;} // when the page is 2, looking at R program, best fit pages will be set to true
+            else if (page == 3){myPrediction[0]  = 1;myPrediction[4]  = 1;myPrediction[10] = 1;}
+            else if (page == 7){myPrediction[0] = 1;myPrediction[8] = 1;myPrediction[9] = 1;}
+            else if (page == 8){myPrediction[0]  = 1;myPrediction[9]  = 1;myPrediction[10] = 1;}
+            else if (page == 10){myPrediction[0]  = 1;myPrediction[11] = 1;myPrediction[12] = 1;}
+            else if (page == 11){myPrediction[0]  = 1;myPrediction[1]  = 1;myPrediction[12] = 1;}
+            else if (page == 12){myPrediction[0]  = 1;myPrediction[9]  = 1;myPrediction[13] = 1;}
+            else if (page == 13){myPrediction[0]  = 1;myPrediction[9]  = 1;myPrediction[10] = 1;myPrediction[14] = 1;}
+            else if (page == 14){myPrediction[0] = 1;myPrediction[2] = 1;}
+            else{myPrediction[0] = 1;myPrediction[page + 1] = 1;myPrediction[page + 2] = 1;} // this is accounting for the default case which are pages 0,1,4,5,6
+            int numPages = 0;
+            while(numPages < 15)
+            {
+                if (myPrediction[numPages] == 0){pageout(CurProcess,numPages);}
+                if (myPrediction[numPages] == 1){pagein(CurProcess,numPages);}
+                numPages++;
+            }
+        }
+        CurProcess++;
     }
-    
-    /* TODO: Implement Predictive Paging */
-    fprintf(stderr, "pager-predict not yet implemented. Exiting...\n");
-    exit(EXIT_FAILURE);
-
-    /* advance time for next pageit iteration */
-    tick++;
 } 
